3adaf9beab41ec5e00f66aeb2f576da2cee9f6ad
WhiteSpace <78596001+whitespace-dpo@users.noreply.github.com>
2023-11-03 19:17:45 +0700

    user account flow

---
+++ app/src/main/java/com/example/e_commerce/Activity/UserManageOrderActivity.java
@@ -0,0 +1,16 @@
+package com.example.e_commerce.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.e_commerce.R;
+
+public class UserManageOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_manage_order);
+    }
+}
